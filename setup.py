a5c73b7191f27fcdac1c95e3e44ef49afd3e9d07
Varun Saxena <varunsaxena5elc@gmail.com>
2025-08-13 13:52:16 +0530

    project structure and venv ready

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Varun Saxena",
+    author_email="varunsaxena5elc@gmail.com",
+    packages=find_packages()
+)
